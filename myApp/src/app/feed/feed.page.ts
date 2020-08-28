4e22d76e443cba841fa6665778e92c03dad4f6d8
Gustavo Antunes <GuAntunes@github.com>
2020-08-27 23:59:36 -0300

    Criado uma página de feed

--- myApp/src/app/feed/feed.page.ts
+++ myApp/src/app/feed/feed.page.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedPage implements OnInit {
 
+  nomeUsuario:string = "Gustavo Antunes";
+
   constructor() { }
 
   ngOnInit() {
